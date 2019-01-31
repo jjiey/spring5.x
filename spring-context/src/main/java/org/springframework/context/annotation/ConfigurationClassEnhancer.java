6ff373becc64aeae02b3b7fab2cbfea9085ebd93
jjiey <471419897@qq.com>
2019-02-01 00:31:12 +0800

    just commit

--- spring-context/src/main/java/org/springframework/context/annotation/ConfigurationClassEnhancer.java
+++ spring-context/src/main/java/org/springframework/context/annotation/ConfigurationClassEnhancer.java
@@ -126,10 +126,11 @@ class ConfigurationClassEnhancer {
 	private Enhancer newEnhancer(Class<?> configSuperClass, @Nullable ClassLoader classLoader) {
 		Enhancer enhancer = new Enhancer();
 		//增强父类，地球人都知道cglib是基于继承来的
-		// 将当前类作为父类
+		// 所以将当前类作为父类
 		enhancer.setSuperclass(configSuperClass);
 		//增强接口，为什么要增强接口?
 		//便于判断，表示一个类以及被增强了
+		//表示代理类实现了这个接口
 		enhancer.setInterfaces(new Class<?>[] {EnhancedConfiguration.class});
 		//不继承Factory接口
 		enhancer.setUseFactory(false);
@@ -141,7 +142,7 @@ class ConfigurationClassEnhancer {
 		//有了factory就能获得对象，而不用去通过方法获得对象了，因为通过方法获得对象不能控制器过程
 		//该BeanFactory的作用是在this调用时拦截该调用，并直接在beanFactory中获得目标bean
 		enhancer.setStrategy(new BeanFactoryAwareGeneratorStrategy(classLoader));
-		//过滤方法，不能每次都去new
+		//添加了一个方法的过滤器，过滤方法，不能每次都去new
 		enhancer.setCallbackFilter(CALLBACK_FILTER);
 		enhancer.setCallbackTypes(CALLBACK_FILTER.getCallbackTypes());
 		return enhancer;
